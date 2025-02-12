c46512004ff464a81fe4805cfae2cc6209dafabc
AntoScher <asc8er@gmail.com>
2025-02-12 14:44:42 +0300

    add flower page

--- finprosto/general/views.py
+++ finprosto/general/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>Это home страница моего итогового проекта на Django </h1>")
 
-
+def flower(request):
+    return HttpResponse("<h1>Это страница c каталогом цветов </h1>")
 from django.shortcuts import render
 
 # Create your views here.
