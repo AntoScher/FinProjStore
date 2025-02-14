7d100a792df6e5d9a83834c007f7fab73889f78f
AntoScher <asc8er@gmail.com>
2025-02-14 14:51:55 +0300

    add html

--- flo-del-sto/general/views.py
+++ flo-del-sto/general/views.py
@@ -1,40 +1,31 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect
+from django.shortcuts import render
 import re
-
+#from .models import Flower
 
 def index(request):
-    return HttpResponse("<h1>Это home страница моего итогового проекта на Django </h1>")
+    return render(request, 'general/index.html')
 
 def flower(request,flower_id=None):
  #   №def flower(request,flower_id=None):
     if flower_id is None:
-        return HttpResponse(
-            "<h1>Это страница каталога цветов</h1>"
-            "<p>Выберите №(id) цветка или его обозначение (slug) для получения дополнительной информации.</p>")
+        #all_books = Flower.objects.all()
+        return render(request, 'general/flowers.html')
     if flower_id>2:
         return redirect('/')
-
-    return HttpResponse(f"<h1>Это страница c каталогом цветов </h1><p>id: {flower_id}</p>")
+    return HttpResponse("<p>Выберите №(id) цветка или его обозначение (slug) для получения дополнительной информации.</p>")
+#HttpResponse(f"<h1>Это страница c каталогом цветов </h1><p>id: {flower_id}</p>")
 
 def flower_slug(request,flower_slug):
     # Регулярное выражение для проверки slug
     valid_slug_pattern =  r'^[a-zA-Z0-9_-]+$'
-
     # Проверяем, соответствует ли flower_slug регулярному выражению
     if not re.match(valid_slug_pattern, flower_slug):
-     return redirect('/')
-
-    return HttpResponse(f"<h1>Это страница c каталогом цветов </h1><p>slug: {flower_slug}</p>")
-
-
+        return redirect('/')
+    return render(request, 'general/flowers.html')
+#return HttpResponse(f"<h1>Это страница c каталогом цветов </h1><p>slug: {flower_slug}</p>")
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
-
-"""  эта хренъ почему то не работает в if 
-    # Если slug не совпадает, возвращаем сообщение об ошибке
-          return HttpResponse(
-            "<h1>Проверьте адрес страницы</h1>"
-            "<p>slug: {}</p>".format(flower_slug)) """
