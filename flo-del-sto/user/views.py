431f60f8dcb787494bafcf4a4852187ade51fd84
AntoScher <asc8er@gmail.com>
2025-02-14 16:16:29 +0300

    shablons

--- flo-del-sto/user/views.py
+++ flo-del-sto/user/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 
 
 def user_aut(request):
     return render(request, 'user/user_aut.html')
 
 #def user_aut(request):     return HttpResponse("<h1>Это начальная страница модуля авторизации</h1>")
-#C:\DEV\GitHupRepo\FinProjStore\flo-del-sto\templates\base.html
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
