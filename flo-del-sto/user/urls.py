63bf25ff0c7fc9a196fd8c08745a1d1da4901ef7
AntoScher <asc8er@gmail.com>
2025-02-18 16:44:59 +0300

    user1

--- flo-del-sto/user/urls.py
+++ flo-del-sto/user/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 path('', views.user_aut, name='autor'),
-	#path('user/', views.user_aut, name='autor'),
 
+path('user_aut/', views.user_aut, name='autor'),
+#path('user_aut/', views.user_aut, name='user_aut'),
 ]
