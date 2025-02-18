a9ad328541d2f91db469171050cb55822700db9d
AntoScher <asc8er@gmail.com>
2025-02-18 20:52:47 +0300

    tsart autor

--- flo-del-sto/user/urls.py
+++ flo-del-sto/user/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.user_aut, name='autor'),
-
 path('user_aut/', views.user_aut, name='autor'),
-#path('user_aut/', views.user_aut, name='user_aut'),
+
 ]
--- flo-del-sto/user/views.py
+++ flo-del-sto/user/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponseNotFound
 
 
 def user_aut(request):
-    return render(request, 'user/user_aut.html')
+    # Добавляем переменную caption1 в контекст
+    context = {'caption1': 'Цветочного магазина'}  # Значение переменной
+
+    return render(request, 'user/user_aut.html', context)
 
 #def user_aut(request):     return HttpResponse("<h1>Это начальная страница модуля авторизации</h1>")
 def page_not_found(request, exception):
